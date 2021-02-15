9ed95b6eb068b22d86eba9dce5d88b1fdcb57999
zackgithuboriginal <z.gibbons@hotmail.com>
2021-02-15 13:26:36 +0000

    Imported werkzeug to generate password hashes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
