595f292d506874d74af93e98019ba5c4df677947
zackgithuboriginal <z.gibbons@hotmail.com>
2021-02-26 17:19:54 +0000

    Added post options display button with funcitonality

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $("#date-target").text(new Date().getFullYear()); 
 });
+
+function displayOptions(postId){
+    target = document.getElementById(`post-options-${postId}`)
+    if (target.style.display  == "flex"){
+        target.style.display  = "none"
+    } else {
+        target.style.display  = "flex"
+    }
+}
