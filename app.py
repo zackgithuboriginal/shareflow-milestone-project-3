6200dce88746044f18d22bd501c29ec08082b789
zackgithuboriginal <z.gibbons@hotmail.com>
2021-03-23 15:44:49 +0000

    Undid previous erroneous commit

--- app.py
+++ app.py
@@ -275,14 +275,21 @@ def account(username):
         user = mongo.db.users.find_one(
             {"username": session["user"]})
         username = user["username"]
+        plussedPostIds = user["voted"]
+        userPlusses = []
+        for post in plussedPostIds:
+            userPlusses.append(
+                mongo.db.posts.find_one({"_id": ObjectId(post)}))
         userPosts = list(
             mongo.db.posts.find({"author": session["user"]}))
         userComments = list(
             mongo.db.posts.find(
                 {"comments.author": session["user"]}))
+        print(session["user"])
         return render_template(
             "account.html", username=username,
-            userPosts=userPosts, userComments=userComments)
+            userPosts=userPosts, userComments=userComments,
+            userPlusses=userPlusses)
 
     return redirect(url_for("login"))
 
